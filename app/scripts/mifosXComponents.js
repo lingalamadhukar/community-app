4946e69ad64974153b3b6f73c5b37ee7874686af
Madhukar <madhukar@confluxtechnologies.com>
2014-09-25 04:47:20 +0530

    Define Office Opening balances

--- app/scripts/mifosXComponents.js
+++ app/scripts/mifosXComponents.js
@@ -208,7 +208,8 @@ define(['Q', 'underscore', 'mifosX'], function (Q) {
             'main/ProfileController',
             'main/ViewMakerCheckerTaskController',
             'main/AdHocQuerySearchController',
-            'accounting/AccOGMController'
+            'accounting/AccOGMController',
+            'accounting/DefineOpeningBalancesController'
         ],
         filters: [
             'StatusLookup',
